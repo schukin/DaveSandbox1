1bac6b930bf9b00d4cac580f624126110b16f846
Dave Schukin <daveschukin@gmail.com>
2016-11-08 16:26:24 -0800

    Initial Commit

---
+++ DaveSandbox1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DaveSandbox1
+//
+//  Created by David Schukin on 11/8/16.
+//  Copyright © 2016 schukin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
